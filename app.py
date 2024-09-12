e3044955f8edd7d8edaa34d47c3833bcd8380131
Keegan <keeganpatton@gmail.com>
2024-09-11 20:27:12 -0500

    add kpi cards

--- app.py
+++ app.py
@@ -31,8 +31,30 @@ bill_list = viz_data['Title'].unique()
 
 # KPI Card slices
 # .loc ["Row Number", "Column to start from":]
-grocery_avg = df.iloc[2, 1:].mean()
+flex_spend_avg = df.iloc[23, 1:].mean().round(2)
+electric_avg = df.iloc[6, 1:].mean().round(2)
+grocery_avg = df.iloc[2, 1:].mean().round(2)
+water_avg = df.iloc[10, 1:].mean().round(2)
 
+flex_spend_current_month = df.iloc[23, -1:].max().round(2)
+electric_current_month = df.iloc[6, -1:].max().round(2)
+grocery_current_month = df.iloc[2, -1:].max().round(2)
+water_current_month = df.iloc[10, -1:].max().round(2)
+
+
+def find_percent_variance(curr_val, past_val):
+    
+    var = curr_val - past_val
+    diff = ((var / past_val) * 100).round(1)
+    val = f"{diff}%"
+    return val
+
+
+# KPI Card Current variance values
+flex_spend_current_month_var = find_percent_variance(flex_spend_current_month, flex_spend_avg)
+electric_curr_month_var = find_percent_variance(electric_current_month, electric_avg)
+grocery_current_month_var = find_percent_variance(grocery_current_month, grocery_avg)
+water_current_month_var = find_percent_variance(water_current_month, water_avg )
 
 
 
@@ -41,10 +63,26 @@ grocery_avg = df.iloc[2, 1:].mean()
 ######################
 def main():
     
-    st.title("Patton Family Budget")
+
+    #####################
+    #     KPI CARDS     #
+    #####################
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        st.metric(value=grocery_avg, label="Avg Grocery", delta=grocery_current_month_var, delta_color='inverse')
+
+    with col2:
+        st.metric(value=electric_avg, label="Avg Electric", delta=electric_curr_month_var, delta_color='inverse')
     
-    
-    st.metric(value=grocery_avg, label="Avg Grocery")
+    with col3:
+        st.metric(value=water_avg, label="Avg Water", delta=water_current_month_var, delta_color='inverse')
+
+    with col4:
+        st.metric(value=flex_spend_avg, label="Avg Flex Spend", delta=flex_spend_current_month_var, delta_color='inverse')
+   
+
+    st.write("____________________")
 
     ######################
     #      Sidebar       #
